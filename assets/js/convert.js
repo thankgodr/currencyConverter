dfe332f1ee969173eabe722c195422e05333e8c7
THANKGOD RICHARD <madueketf@gmail.com>
2016-05-21 09:51:18 +0100

    testing success on submit

--- assets/js/convert.js
+++ assets/js/convert.js
@@ -11,29 +11,27 @@ $(document).ready(function(){
 		    	 $('#convert_to').append($("<option></option>")
                     .attr("value",key)
                     .text(key));
+           console.log("hi")
 		    }
 	}
 	});
    var fro = 0;
    var to = 0;
+ 
 
     
 	$("#formconvert").submit(function(e) {
     var amount = $("#amount").val();
     var convFrom = $('#convert_from option:selected').val();
     var convTo =  $('#convert_to option:selected').val();
-    var url = "https://openexchangerates.org/api/latest.json?app_id=7c3a11e894794c4e91be3e449d4b7c86"
+    var url = "https://openexchangerates.org/api/latest.json?app_id=7c3a11e894794c4e91be3e449d4b7c86&base=" + convFrom;
     $.ajax({
            type: "GET",
            url: url,
            success: function(data)
            {
-           	console.log(data)
-           	data.base = convFrom
-           	fro = data.base
-            to = data.rates[convTo];
-            var base = data.rates["USD"]
-            alert(convFrom +" "+ amount+" in "+ convTo +" = "+ fro * to);
+           	console.log(data.rates)
+
             }
          });
       
